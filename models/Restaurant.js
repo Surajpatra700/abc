b221d6e9f325af1292bb05c4f7a550e12fac394b
Suraj Patra <surajpatra2908@gmail.com>
2023-11-04 18:46:15 +0530

    sggsg

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,23 +1,19 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true
     },
     email: {
         type: String,
-        required: true,
-        unique: true
+        required: true,  
     },
     password: {
         type: String,
         required: true
-    },
-    
+    }, 
 });
 
 
-const Register = new mongoose.model("Register", userSchema);
-
-module.exports = Register;
+module.exports =  mongoose.model("User", UserSchema);
--- router/UserData.js
+++ router/UserData.js
@@ -1,72 +1,87 @@
-const express=require('express');
-const router=express.Router();
-const bcrypt=require('bcryptjs');
+const express = require('express');
+const route = express.Router();
+const bcrypt=require('bcrypt');
 const jwt=require("jsonwebtoken");
-const {body,validationResult}=require("express-validator");
+const SECRET_KEY = "sshvs";
+// const {body,validationResult}=require("express-validator");
 const jwtSecret=process.env.JWT_SECRET;
 
 
-const Register=require('../models/Restaurant');
+const User = require("../models/Restaurant");
 
 
-router.post("/createuser",[
-    body('email','incorrect email').isEmail(),
-    body('username').isLength({min:5}),
-    body('password','incorrect password').isLength({min:5})],
-    
-    async (req, res) => {
-    const errors=validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).send({errors:errors.array()})
+// const errors = validationResult(req);
+
+// if (!errors.isEmpty()) {
+//   console.log(errors);
+//   return res.status(400).json({ errors: errors.array() });
+// }
+
+
+
+
+route.post("/createuser", async (req, res) => {
+  try {
+    // Check if the required fields are present in the request body
+    if (!req.body.username || !req.body.email || !req.body.password) {
+      return res.status(400).json({ error: "Missing required fields" });
     }
-    
-    const salt=await bcrypt.genSalt(10);
-    let secPassword=await bcrypt.hash(req.body.password,salt)
-    
-        try{
-        await Register.create({
-            username: req.body.username,
-            password: secPassword,
-            email: req.body.email,
-        }).then(res.send({success:true}));
-    }catch(err){
-        console.log(err);
-        res.send({success:false});
-    }
+
+    const {username,email,password}=req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      username,
+      email,
+      password:hashedPassword
     });
 
+    const createuser = await newUser.save();
+    console.log("Successfully saved!");
+    res.status(201).send(createuser);
+  } catch (err) {
+    console.log(err);
+    // Handle the error appropriately, e.g., send an error response
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
-    router.post("/loginuser", async (req, res) => {
-        const { email, password } = req.body;
-      
-        try {
-          const userData = await Register.findOne({ email });
-      
-          if (!userData) {
-            return res.status(401).json({ errors: "Invalid login credentials" });
-          }
-      
-          // Now that you have found a user, you can proceed to check the password.
-          const pwdCompare = await bcrypt.compare(password, userData.password);
-      
-          if (!pwdCompare) {
-            return res.status(401).json({ errors: "Invalid login credentials" });
-          }
-      
-          const data = {
-            user: {
-              id: userData.id
-            }
-          };
-      
-          const authToken = jwt.sign(data, jwtSecret);
-          return res.json({ success: true, authToken: authToken });
-        } catch (err) {
-          console.error(err);
-          res.status(500).json(err,{ success: false });
-        }
-      });
-      
 
-    module.exports=router;
+
+route.post("/loginuser", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userData = await User.findOne({ email: email });
+
+    if (!userData) {
+      return res.status(401).json({ errors: "Invalid login credentials" });
+    }
+
+    // Now that you have found a user, you can proceed to check the password.
+    const pwdCompare = await bcrypt.compare(password, userData.password);
+
+    const data = {
+      User: {
+        id: userData._id
+      }
+    };
+
+    if (pwdCompare) {
+    const authToken = jwt.sign({email:userData.email,password:userData.password}, jwtSecret);
+    return res.json({ success: true, authToken: authToken });
+    }
+    else{
+      return res.status(401).json({ errors: "Invalid login credentials due to password" });
+    }
+
     
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err, { success: false });
+  }
+});
+
+
+module.exports = route;
